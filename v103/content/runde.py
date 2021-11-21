eaf7db5bba35bf4eea5e1938628d3c957bf231ed
Annika Bennemann <annika.bennemann@tu-dortmund.de>
2021-11-21 22:46:31 +0100

    hab nen Plot mit unsern Daten geschafft

---
+++ v103/content/runde.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+ 
+ 
+x, y = np.loadtxt('Runder_Stab_eins.txt', unpack=True,delimiter=',')
+
+z=(530*x**2-(x**3//3))*10**-6
+ 
+def f(z,a,b):
+    return a*z+b
+params, cov = curve_fit(f, z, y)
+errors = np.sqrt(np.diag(cov))
+ 
+print('a =', params[0], '±', errors[0])
+print('b =', params[1], '±', errors[1])
+
+x_plot = np.linspace(0,100,10)
+ 
+plt.figure(1)
+plt.plot(z, y,'rx', label='Messdaten')
+plt.plot(x_plot, f(x_plot,*params),'-', label='Linearer Fit')
+
+
+plt.ylabel('$D(x)$ $[10^{-3} m]$')
+plt.xlabel(r'$(Lx^2 - \frac{x^3}{3})$ $\left[10^{-3} m^3\right]$')
+plt.grid()
+plt.legend(loc='best')
+ 
+ 
+plt.savefig('Runder_Stab_eins.pdf')
