c3ac3f8562e91c186a7fda60bbe94db8e2920ae3
Annika Bennemann <annika.bennemann@tu-dortmund.de>
2021-12-04 14:25:02 +0100

    plot

--- v302/plot.py
+++ v302/plot.py
@@ -1,21 +1,39 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import uncertainties.unumpy as unp
+from uncertainties.unumpy import (nominal_values as noms,
+                                  std_devs as stds)
+from scipy import stats
+from scipy.optimize import curve_fit
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
+v, U_Br = np.genfromtxt('content/Messwerte.txt', unpack=True)
+
+v_0 = 241.14
+y = U_Br/10 #U_Br / U_S
+
+def f(w):
+    return np.sqrt((w**2-1)**2/(9*((1-w**2)**2+9*w**2)))
+
+    
+#### noch rausfinden v_0, cov = curve_fit(f, v, U, p0=(v_0))
+
+x=np.linspace(0.07,100, 20000)
+z=(f(x))
+
+
+plt.figure()
+plt.plot(v/v_0, y,'rx', label='Messdaten')
+plt.plot(x,np.sqrt((x**2-1)**2/(9*((1-x**2)**2+9*x**2))) , 'c', label='Theoretische Werte')
+plt.xscale('log')
+plt.xlabel(r'$\Omega = \nu \mathbin{/} \nu_0$')
+plt.ylabel(r'$U_Br \mathbin{/} U_S$')
 plt.legend(loc='best')
+plt.grid()
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-plt.legend(loc='best')
 
 # in matplotlibrc leider (noch) nicht möglich
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
 plt.savefig('build/plot.pdf')
+
+
---
+++ v302/plottest.py
@@ -0,0 +1,109 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import uncertainties.unumpy as unp
+from uncertainties.unumpy import (nominal_values as noms,
+                                  std_devs as stds)
+from scipy import stats
+from scipy.optimize import curve_fit
+import scipy.constants as const
+
+
+#####################################################################################
+
+
+
+#######################################################################################
+
+#b)
+print('###########################################################')
+print('b)') 
+R3 = 632
+R2 = 228.5
+R4 = 1000-R3
+C2 = 750*10**(-9)
+
+R_x=R2*R3/R4
+print(f"R_x: ", R_x)
+C_x=C2*R4/R3
+print(f"C_x: ", C_x)
+
+R_theo = 464.9
+R_abw = (R_x - R_theo)/R_theo
+C_theo = 433.7
+C_abw = (C_x - C_theo)/C_theo
+
+#########################################################################################
+
+#c)
+print('###########################################################')
+print('c)') 
+R3 = 608
+R2 = 224
+R4 = 1000-R3
+L2 = 0.0201
+
+R_x=R2*R3/R4
+print(f"R_x: ", R_x)
+L_x=L2*R4/R3
+print(f"L_x: ", L_x)
+
+R_theo = 360.5
+R_abw = (R_x - R_theo)/R_theo
+L_theo = 433.7
+L_abw = (L_x - L_theo)/L_theo
+
+#########################################################################################
+
+#d)
+print('###########################################################')
+print('d)') 
+R3 = 191
+R2 = 332
+R4 = 179
+C2 = 750 * 10**(-6)
+
+R_x=R2*R3/R4
+print(f"R_x: ", R_x)
+L_x=L2*R2*R3
+print(f"L_x: ", L_x)
+
+R_theo = 360.5
+R_abw = (R_x - R_theo)/R_theo
+L_theo = 433.7
+L_abw = (L_x - L_theo)/L_theo
+
+###########################################################################################
+
+#e)
+print('###########################################################')
+print('e)') 
+
+v, U= np.genfromtxt('data.txt',unpack=True)
+U=U/2
+table ={'1': v, '2':U}
+print(tabulate (table, tablefmt="latex"))
+U=U/10
+R=1000
+C=295*10**(-9)
+v_0 = 1/(R*C)
+print(v_0)
+
+def hui(omma, a):
+    return np.sqrt(((omma/a)**2-1)**2/(9*((1-(omma/a)**2)**2+ 9* (omma/a)**2)))
+
+v_0, cov = curve_fit(hui, v, U, p0=(v_0))
+print(v_0)
+x=np.linspace(v[0], v[20], 10000)
+y=hui(x, v_0)
+
+plt.figure()
+plt.plot(v/v_0, U, 'b*', label='Messdaten')
+plt.plot(x/v_0, y, 'c')
+plt.xscale('log')
+plt.xlabel(r'$v/v_0$')
+plt.ylabel(r'$U_{Br}/U_{Sp}$')
+plt.legend(loc='best')
+
+
+#Klirrfaktor
+print(f"Klirrfaktor: ", U[8]/hui(2, 1))
