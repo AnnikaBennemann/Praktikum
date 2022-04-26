0843b58489a1c623990318c01d751b67c97cbd75
Paulin Vehling <paulin.vehling@tu-dortmund.de>
2022-04-26 12:21:05 +0200

    merge

--- v704/plot.py
+++ v704/plot.py
@@ -4,6 +4,7 @@ from scipy.signal import find_peaks
 import uncertainties.unumpy as unp
 from uncertainties import ufloat
 from scipy.optimize import curve_fit
+import tools
 
 d , t, N, sigmaN = np.genfromtxt('content/WerteEisen.txt', unpack=True)
 
@@ -20,3 +21,14 @@ sigmaNtb=sigmaNb/tb
 
 print('Ntb= ',Ntb)
 print('sigmaNtb= ',sigmaNtb)
+
+
+#Betastrahlung
+
+d, t, N, Nerror, A, Aerror= np.genfromtxt('content/beta.txt', unpack=True)
+R = d*1e-6 # Massenbelegung
+
+
+
+print('a1 =', a1)
+print('b1 =', b1)
