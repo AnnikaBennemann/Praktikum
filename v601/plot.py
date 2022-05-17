50bddfe99033625c5b6ac31a33369acc94a4521e
Paulin Vehling <paulin.vehling@tu-dortmund.de>
2022-05-17 12:27:31 +0200

    Test

--- v601/plot.py
+++ v601/plot.py
@@ -5,7 +5,13 @@ import uncertainties.unumpy as unp
 from uncertainties import ufloat
 from scipy.optimize import curve_fit
 
+#Weglänge
 T = np.genfromtxt('content/Temperatur.txt', unpack=True)
 
-
+T = T+273.15
+print("T = ", T)
+ps = 5.5 * 10**7 * np.exp(-6876/T)
+print("psät= " , ps)
+w =0.0029/ps
+print("w = ", w)
 #Weglänge ende
