08514f33599b078cf6a47bf7dfa69634e6330a15
Annika Bennemann <annika.bennemann@tu-dortmund.de>
2022-05-17 12:29:11 +0200

    jfllsjf

--- v601/plot.py
+++ v601/plot.py
@@ -8,21 +8,8 @@ from scipy.optimize import curve_fit
 #Weglänge
 T = np.genfromtxt('content/Temperatur.txt', unpack=True)
 
-<<<<<<< HEAD
-T = T+273.15
-print("T = ", T)
-ps = 5.5 * 10**7 * np.exp(-6876/T)
-print("psät= " , ps)
-w =0.0029/ps
-print("w = ", w)
-#Weglänge ende
-||||||| 8d653d6
-
-#Weglänge ende
-=======
 
 #Weglänge ende
 
 
 ########
->>>>>>> f548493a7ad1b0c1b3e1fbac61cf5a70b5bb2df5
