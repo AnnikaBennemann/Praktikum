222e44e1ecf1ef8aa2b65f3f1cf62a368a7a1908
Annika Bennemann <annika.bennemann@tu-dortmund.de>
2022-06-09 17:46:31 +0200

    ich hasse diesen Versuch

--- v503/plot.py
+++ v503/plot.py
@@ -3,7 +3,10 @@ import numpy as np
 from scipy.signal import find_peaks
 import uncertainties.unumpy as unp
 from uncertainties import ufloat
+from uncertainties import umath
+from uncertainties.unumpy import (nominal_values as noms, std_devs as stds)
 from scipy.optimize import curve_fit
+from cmath import sqrt
 
 U, R, t0, tauf, tauff, tab, tabf  = np.genfromtxt('content/Werte.txt', unpack=True)
 
@@ -55,10 +58,70 @@ print('Temp = ', Temp)
 eta= 0.0048 * Temp + 1.7268 
 print('eta = ', eta)
 
+d = ufloat(0.0076250 ,0.0000051) # [m]
+dichte_luft = 1.1644        # näherungsweise
+dichte_oel = 886     # [kg/m^3]
+B = 6.17e-5    # [Pa/m]
+p = 760       # [Pa]
+s = 0.0005      # [m]
 
-#r_oel = np.sqrt(9 * visko_gemessen * v_0 / (2 * g * dichte_oel))???
-#r_korrigiert = (np.sqrt((B / (2 * p))**2 + 9 * visko_gemessen * v_0 / (2 * g * dichte_oel)) - B / (2 * p))???
-#q  = 4 * np.pi / 3 * dichte_oel * r_korrigiert**3 * g * 1 / E_feld ???
+vab= np.delete(vab,[0,1,12,14,21])
+vauf= np.delete(vauf,[0,1,12,14,21])
+eta= np.delete(eta,[0,1,12,14,21])
+E = U/d
+#print('v = ',v)
+
+print('r_oel :')
+i = 0
+while i < 20:
+    r_oel = umath.sqrt(9 * eta[i] * (vab[i]-vauf[i]) / (2 * 9.81 * (dichte_oel-dichte_luft)))
+    print(r_oel)
+    i  += 1
+
+print('q :')
+i = 0
+while i < 20:
+    q = 3*np.pi*eta[i]*umath.sqrt((9*eta[i]*(vab[i]-vauf[i]))/(4*9.81*(dichte_oel-dichte_luft)))*((vab[i]+vauf[i])/E[i])
+    print(q)
+    i  += 1
+
+x, r, rerr, q , qerr, q_korr, q_korrerr= np.genfromtxt('content/plotzeug.txt', unpack=True)
+r= unp.uarray(r,rerr)
+qe= unp.uarray(q, qerr)
+
+eta_korr = eta*(1/(1+B/(p*r*10**(-6))))
+print('eta_korr = ',eta_korr)
+
+q_1= qe*(1+B/(p*r*10**(-6)))**(3/2)
+print('q_korr = ', q_1)
 
 
 
+plt.figure(2)
+plt.errorbar(range(1, len(noms(q)) + 1), np.sort(noms(q)), yerr= qerr, fmt='rx', label='Werte mit Fehlern')
+#plt.plot(range(1, len(noms(q)) + 1), np.sort(noms(q)), 'kx', label = r'Messdaten')
+plt.ylabel(r'q $\mathbin{/} 10^{-19} \si{\coulomb}$')
+plt.xlabel(r'Messungen')
+plt.grid()
+plt.legend(loc='best')
+plt.savefig('build/plot2.pdf')
+
+q= np.sort(noms(q))/ 1.602
+q_mittel = np.array([np.mean(q[0:5]), q[6], np.mean(q[7:8]), np.mean(q[9:12]), np.mean(q[13:14]), np.mean(q[15:16]), q[17], q[18], q[19]])
+print('q_mittel = ',q_mittel)
+print(np.argmin(abs(q / 1.602 - 1)))
+
+
+plt.figure(3)
+plt.errorbar(range(1, len(noms(q_korr)) + 1), np.sort(noms(q_korr)), yerr= q_korrerr, fmt='rx', label='Werte mit Fehlern')
+#plt.plot(range(1, len(noms(q_korr)) + 1), np.sort(noms(q_korr)), 'kx', label = r'Messdaten')
+plt.ylabel(r'q(korrigiert) $\mathbin{/} 10^{-19} \si{\coulomb}$')
+plt.xlabel(r'Messungen')
+plt.grid()
+plt.legend(loc='best')
+plt.savefig('build/plot3.pdf')
+
+q_korr= np.sort(noms(q_korr))/ 1.602
+q_mittel = np.array([np.mean(q_korr[0:5]), q_korr[6], np.mean(q_korr[7:8]), np.mean(q_korr[9:12]), np.mean(q_korr[13:14]), np.mean(q_korr[15:16]), q_korr[17], q_korr[18], q_korr[19]])
+print('q_mittel = ',q_mittel)
+print(np.argmin(abs(q / 1.602 - 1)))
