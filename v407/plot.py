81fefd6379324573d943d74bb6d579bbeb082bfa
Paulin Vehling <paulin.vehling@tu-dortmund.de>
2022-06-14 13:02:03 +0200

    senkrecht

--- v407/plot.py
+++ v407/plot.py
@@ -5,21 +5,24 @@ import uncertainties.unumpy as unp
 from uncertainties import ufloat
 from scipy.optimize import curve_fit
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+I_0 = 0.13*10**(-3)
+I_dunkel = 5.9*10**(-9)
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-plt.legend(loc='best')
+g, Is = np.genfromtxt('content/senkrecht.txt', unpack=True)
+g, Ip = np.genfromtxt('content/parallel.txt', unpack=True)
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-plt.legend(loc='best')
+I_s = Is*10**(-6) - I_dunkel
+I_p = Ip*10**(-6) - I_dunkel
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+
+def n_spol(a, E):                                                                       # E = E_r/E_e = sqrt(I_r/I_0)
+    return np.sqrt((2*E*np.cos(2*a)+ E**2 + 1)/(1-2*E + E**2))
+
+n_s = n_spol(g*np.pi/180, np.sqrt(I_s/I_0))
+print("n_s = ", (n_s))
+n_s_mean = np.mean(n_s[n_s < 14])
+print("mean: " ,n_s_mean)
+n_s_std = np.std(n_s[n_s < 14])
+print("std: " ,n_s_std)
+n_s_err = ufloat(n_s_mean, np.std(n_s[n_s < 14]))
+print("s-polarisiert: ", n_s_err)
