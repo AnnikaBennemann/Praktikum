7b3a213f1b3efd02b9f28077e2ea7828b5495c37
Paulin Vehling <paulin.vehling@tu-dortmund.de>
2022-11-16 13:41:33 +0100

    plot

--- FP/v46/plot.py
+++ FP/v46/plot.py
@@ -7,3 +7,24 @@ from scipy.optimize import curve_fit
 
 #Magnetfeld Grafik
 x, B = np.genfromtxt('content/Magnetfeld.txt', unpack=True)
+
+
+plt.figure(1)
+plt.plot(x, B,'rx', label='Messdaten')
+plt.ylabel(r'$B / \si{\milli\tesla}$')
+plt.xlabel(r'$x / \si{\centi\meter}$')
+plt.grid()
+plt.legend(loc='best')
+plt.savefig('build/Magnetfeld.pdf') 
+
+#effektive Masse bestimmen
+
+#winkel normieren
+lu,t1u, t2u  = np.genfromtxt('content/undotiert.txt', unpack=True)
+l12,t112, t212  = np.genfromtxt('content/12.txt', unpack=True)
+l28,t128, t228  = np.genfromtxt('content/28.txt', unpack=True)
+
+#Dicke
+du = 1
+
+t1u = t1u/ 
